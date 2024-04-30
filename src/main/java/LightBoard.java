f1b4782be8b8359175f5c60397d933b9e9a2b0ad
ActualDucky <119700539+ActualDucky@users.noreply.github.com>
2024-04-29 21:27:41 -0700

    LightBoard.java

--- src/main/java/LightBoard.java
+++ src/main/java/LightBoard.java
@@ -13,8 +13,8 @@ public class LightBoard
     lights = new boolean[numRows][numCols];
     for (int r = 0; r < numRows; r++) {
       for (int c = 0; c < numCols; c++) {
-        double rnd = Math.random();
-        lights[r][c] = rnd < 0.4;
+        if (Math.random() <= 0.4)
+          lights[r][c] = true;
       }
     }
   }
